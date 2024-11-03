a74297e6d3326bc444fbf49437cc33ae1b4acfe3
kygrayae <149627471+kygrayae@users.noreply.github.com>
2024-11-03 18:04:12 -0500

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -20,7 +20,6 @@ suite('Unit Tests', function () {
       assert.fail("I'm truthy", 'A string is truthy');
       assert.fail(true, 'true is truthy');
     });
-    
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.fail(true, 'true is true');
